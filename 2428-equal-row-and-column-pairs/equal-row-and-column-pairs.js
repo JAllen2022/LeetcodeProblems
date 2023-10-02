8f70c8e066e8d04636c593b68c29acfeeda013cc
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-10-02 13:34:47 -0700

    Time: 164 ms (28.59%) | Memory: 56.1 MB (30.87%) - LeetSync

---
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var equalPairs = function(grid) {
+
+    const grids= {}
+    let count=0
+
+    for(let i=0;i<grid[0].length;i++) {
+        let temp = ""
+        for(let j=0;j<grid.length-1;j++) {
+            temp+=grid[j][i]+","
+        }
+
+        temp+=grid[grid.length-1][i]
+
+        if (grids[temp]){
+            grids[temp]++
+        } else {
+            grids[temp]=1
+        }
+    }
+
+    console.log("checking grids", grids)
+
+    for(let i=0;i<grid.length;i++) {
+        const temp = grid[i].join(",")
+        console.log("temp", temp)
+        if(grids[temp]){
+            count+=grids[temp]
+        }
+    }
+
+
+    return count
+    
+};
