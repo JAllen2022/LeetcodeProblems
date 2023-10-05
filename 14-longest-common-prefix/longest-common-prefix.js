d4e32e7930d86e3aed7af0446fb9fbe76555cc01
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-10-04 23:43:20 -0700

    Time: 70 ms (5.79%) | Memory: 42.6 MB (42.15%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+
+    if(strs.length<1) return ""
+    
+    let index=0;
+
+    while(index<strs[0].length) {
+        let letter = strs[0][index]
+        for(let i=0;i<strs.length;i++) {
+            let curr = strs[i]
+            if(curr[index]!==letter){
+                return strs[0].slice(0,index)
+            }
+        }
+        index++
+    }
+    console.log("checking index", index)
+    return strs[0].slice(0,index)
+    
+};
