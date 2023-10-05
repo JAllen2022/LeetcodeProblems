f731e4b3b46b4ad74aa042f7c64209a0bdc6cf30
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-10-04 23:45:10 -0700

    Time: 59 ms (34.25%) | Memory: 42.2 MB (69.30%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -2,22 +2,33 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function(strs) {
+var longestCommonPrefix = function(arr) {
 
-    if(strs.length<1) return ""
-    
-    let index=0;
 
-    while(index<strs[0].length) {
-        let letter = strs[0][index]
-        for(let i=0;i<strs.length;i++) {
-            let curr = strs[i]
-            if(curr[index]!==letter){
-                return strs[0].slice(0,index)
-            }
+    let i = 1;
+    let prf = arr[0];
+    while(i < arr.length){
+        if(!arr[i].startsWith(prf)){
+            prf = prf.slice(0, -1)
+        }else{
+            i++
         }
-        index++
     }
-    return strs[0].slice(0,index)
+    return prf
+    // if(strs.length<1) return ""
+    
+    // let index=0;
+
+    // while(index<strs[0].length) {
+    //     let letter = strs[0][index]
+    //     for(let i=0;i<strs.length;i++) {
+    //         let curr = strs[i]
+    //         if(curr[index]!==letter){
+    //             return strs[0].slice(0,index)
+    //         }
+    //     }
+    //     index++
+    // }
+    // return strs[0].slice(0,index)
     
 };
