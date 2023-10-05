9837b4466154fbe320032e0fb0dd8b3ce9b88ae9
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-10-04 23:33:22 -0700

    Time: 104 ms (72.20%) | Memory: 46.4 MB (91.95%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -1,44 +1,25 @@
-/**
- * @param {string} s
- * @return {number}
- */
 var romanToInt = function(s) {
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
-    let myObj = {
-        "M":1000,
-        "CM":900,
-        "D":500,
-        "CD":400,
-        "C":100,
-        "XC":90,
-        "L":50,
-        "XL":40,
-        "X":10,
-        "IX":9,
-        "V":5,
-        "IV":4,
-        "I":1,
-    }
+    let result = 0;
 
-    let num = 0;
-    let index=0;
-    for(let keys in myObj) {
-        let op1 = s[index]
-        let op2 = s[index]+ s[index+1]
-        while(op1===keys || op2===keys){
-            if(keys===op2){
-                num+= myObj[op2]
-                index+=2
-            } else if (keys===op1){
-                num+= myObj[op1]
-                index++
-            }
-            op1 = s[index]
-            op2 = s[index]+ s[index+1]
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currValue = romanValues[s[i]];
+
+        if (i < s.length - 1 && currValue < romanValues[s[i + 1]]) {
+            result -= currValue;
+        } else {
+            result += currValue;
         }
     }
 
-    return num
-
-    
+    return result;
 };
