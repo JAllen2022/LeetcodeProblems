800fe86feb2bf8f064fcb99595718c05dce97a79
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-10-14 14:34:39 -0700

    Time: 67 ms (10.07%) | Memory: 44.5 MB (46.43%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.js
+++ 412-fizz-buzz/fizz-buzz.js
@@ -7,10 +7,13 @@ var fizzBuzz = function(n) {
     const ret = []
 
     for(let i=1;i<=n;i++) {
-        if(i%3===0 && i%5===0) ret.push("FizzBuzz")
-        else if (i%3===0) ret.push("Fizz")
-        else if (i%5===0) ret.push("Buzz")
-        else ret.push(`${i}`)
+        let a = ""
+        if(i%3===0 && i%5===0) a="FizzBuzz"
+        else if (i%3===0) a="Fizz"
+        else if (i%5===0) a="Buzz"
+        else a=`${i}`
+
+        ret.push(a)
     }
 
     return ret
