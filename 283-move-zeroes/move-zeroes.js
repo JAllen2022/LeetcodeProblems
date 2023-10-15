d02e8fb541c5d878aac9bcdd0dbd47f34d4055a7
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-10-15 12:19:33 -0700

    Time: 84 ms (30.18%) | Memory: 46.5 MB (65.74%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -1,22 +1,13 @@
-/**
- * @param {number[]} nums
- * @return {void} Do not return anything, modify nums in-place instead.
- */
-
 var moveZeroes = function(nums) {
-    let left = 0
-    let zeroCount = 0
-    for(let i=0; i<nums.length;i++){
-        if(nums[i]===0){
-            zeroCount++
-        } else {
-            nums[left]=nums[i]
-            left++
+    let left = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            // Swap non-zero element with the leftmost zero
+            [nums[left], nums[i]] = [nums[i], nums[left]];
+            left++;
         }
     }
-
-    for(let i=0;i<zeroCount;i++) {
-        nums[nums.length-1-i]=0
-    }
-    return nums
+    
+    return nums;
 };
