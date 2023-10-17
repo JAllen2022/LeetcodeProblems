f7e0f43a36544b82918ebd02a51a0cf6f49210b3
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-10-17 15:26:47 -0700

    Time: 53 ms (84.55%) | Memory: 43.7 MB (97.47%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.js
+++ 459-repeated-substring-pattern/repeated-substring-pattern.js
@@ -3,24 +3,34 @@
  * @return {boolean}
  */
 var repeatedSubstringPattern = function(s) {
-   let len = 2
-   let found = false
-   while(len<=s.length) {
-       let increment = s.length/len
-       let subString = s.slice(0,increment)
-       let run= true
-       for(let i=increment;i<s.length;i+=increment ) {
-           if(s.slice(i,i+increment)!==subString){
-               run=false
-               break
-           }
-       }
-       if( run){
-           found=true
-            break
-        }
-       len++
-   }
-
-   return found
+    const doubled = s + s;
+    const sub = doubled.slice(1, -1);
+    return sub.includes(s);
 };
+
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// var repeatedSubstringPattern = function(s) {
+//    let len = 2
+//    let found = false
+//    while(len<=s.length) {
+//        let increment = s.length/len
+//        let subString = s.slice(0,increment)
+//        let run= true
+//        for(let i=increment;i<s.length;i+=increment ) {
+//            if(s.slice(i,i+increment)!==subString){
+//                run=false
+//                break
+//            }
+//        }
+//        if( run){
+//            found=true
+//             break
+//         }
+//        len++
+//    }
+
+//    return found
+// };
