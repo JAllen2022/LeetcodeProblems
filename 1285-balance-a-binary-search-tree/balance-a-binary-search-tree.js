d399a6a33572286f8d5efbbbbe427e2dd4cdd051
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-11-10 14:00:09 -0800

    Time: 122 ms (61.21%) | Memory: 63.8 MB (32.76%) - LeetSync

---
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.js
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var balanceBST = function(root) {
+
+    // In order Traversal -> put all values in a list. Split it in half, create BST that way
+    const arr = []
+
+    function inorderTraversal(node) {
+        if(!node) return
+        if(node.left) inorderTraversal(node.left)
+        arr.push(node.val)
+        if(node.right) inorderTraversal(node.right)
+    }
+
+    inorderTraversal(root)
+    console.log(arr)
+
+    const mid= Math.floor(arr.length/2)
+    const newRoot = new TreeNode(arr[mid])
+    
+    insertNodeBST(mid+1,arr.length-1, arr,newRoot)
+    insertNodeBST(0,mid-1, arr,newRoot)
+
+
+
+    return newRoot;
+    
+};
+
+function insertNodeBST (left,right,arr, root) {
+   if(left>right || !root) return
+   const mid = Math.floor((left+right)/2)
+   const val = arr[mid]
+   const tempNode = new TreeNode(val)
+   if(root.val<val) root.right= tempNode
+   if(root.val>val) root.left= tempNode
+
+   insertNodeBST(mid+1,right,arr,tempNode)
+   insertNodeBST(left,mid-1,arr,tempNode)
+
+
+}
