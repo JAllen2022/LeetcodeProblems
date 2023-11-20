ab6a4b476c076e68887e41d99ff87666658a88bb
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-11-20 15:39:36 -0800

    Time: 52 ms (51.52%) | Memory: 41.8 MB (83.23%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var searchInsert = function(nums, target) {
+
+
+    let index=0;
+    let mid = Math.floor(nums.length/2)
+    let left = 0;
+    let right = nums.length-1
+    
+    // check if target is less than or greater than last nodes
+    if(target<nums[0]) return 0
+    if(target>nums[nums.length-1]) return nums.length
+
+    while(left<right) {
+        if(nums[mid]===target) return mid;
+        else {
+            const l = mid-1
+            const r = mid+1
+            console.log("checking", l,mid,r)
+            if(nums[l]<target && target<nums[mid] && nums[l]!==target){
+                return mid
+            } else if (nums[mid]<target && target<nums[r] && nums[r]!==target){
+                return r
+            }
+
+            if (nums[mid]>target){
+                right=mid-1
+            } else {
+                left=mid+1;
+            }
+        }
+        mid = Math.floor((left+right)/2)
+        
+    }
+
+    return mid
+};
