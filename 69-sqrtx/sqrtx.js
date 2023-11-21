315ff4a36a59c7ef2541e189f30c904a739a5e6b
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-11-21 15:50:09 -0800

    Time: 127 ms (5.01%) | Memory: 43.9 MB (31.69%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    return Math.floor(Math.sqrt(x))
+    if(x===0) return 0
+    for(let i=1;i<=x;i++) {
+        let check = i*i
+        let rem = x-check
+        if(check>x) return i-1
+        if(rem===0 || rem/i <= 1 ) return i
+    }
 };
