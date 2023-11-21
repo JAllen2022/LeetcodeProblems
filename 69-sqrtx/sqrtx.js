90cd3a704aad6b0d4c1079a7cc7d8c02166ae6f8
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-11-21 15:54:40 -0800

    Time: 57 ms (81.72%) | Memory: 43.7 MB (56.66%) - LeetSync

--- 69-sqrtx/sqrtx.js
+++ 69-sqrtx/sqrtx.js
@@ -3,11 +3,24 @@
  * @return {number}
  */
 var mySqrt = function(x) {
-    if(x===0) return 0
-    for(let i=1;i<=x;i++) {
-        let check = i*i
-        let rem = x-check
-        if(check>x) return i-1
-        if(rem===0 || rem/i <= 1 ) return i
+
+    let left = 0;
+    let right = x
+
+    while(left<=right) {
+        const mid = Math.floor((right+left)/2)
+        const sqrt= mid*mid
+        if( (sqrt<=x) && (mid+1)*(mid+1)>x) return mid
+        else if (sqrt>x) right = mid-1
+        else left = mid+1
     }
+    
 };
+
+// if(x===0) return 0
+//     for(let i=1;i<=x;i++) {
+//         let check = i*i
+//         let rem = x-check
+//         if(check>x) return i-1
+//         if(rem===0 || rem/i <= 1 ) return i
+//     }
