0323920ad3271f94fe7b2aaaab847cc6e1b8afa0
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-11-22 11:25:01 -0800

    Time: 440 ms (6.03%) | Memory: 107.3 MB (5.10%) - LeetSync

---
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.js
@@ -0,0 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var pairSum = function(head) {
+
+    let slow = head
+    let fast = head.next
+
+    while(fast.next) {
+        slow=slow.next;
+        fast=fast.next.next
+        
+    }
+
+    console.log(slow)
+
+    let temp = slow.next
+    slow.next=null;
+    let secondHalf;
+    let prev=null;
+
+    while(temp){
+        if(!temp.next) secondHalf=temp
+        let tempTemp = temp.next;
+        temp.next=prev
+        prev=temp
+        temp = tempTemp
+    }
+
+    console.log(secondHalf)
+    let firstHalf = head
+    let max = -Infinity
+
+    while(firstHalf) {
+
+        max = Math.max(firstHalf.val+secondHalf.val,max)
+        firstHalf=firstHalf.next;
+        secondHalf=secondHalf.next;
+
+    }
+    
+    return max
+    
+};
+
+
+//   let arr = []
+
+//     while(head){
+//         arr.push(head.val)
+//         head=head.next
+//     }
+//     let max=0
+//     for(let left = 0, right=arr.length-1; left <right;left++,right--){
+//         max= Math.max(arr[left] + arr[right], max) 
+//     }
+
+//     return max
