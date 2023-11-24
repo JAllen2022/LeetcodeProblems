e893a5bb29264afe444425ce9969a2d5e139a0c5
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-11-23 17:30:51 -0800

    Time: 87 ms (19.56%) | Memory: 48.8 MB (88.22%) - LeetSync

--- 344-reverse-string/reverse-string.js
+++ 344-reverse-string/reverse-string.js
@@ -3,5 +3,15 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    return s.reverse()
+
+    right=s.length-1
+
+    for(let i=0;i<s.length && i<=right;i++) {
+        let temp=s[i]
+        s[i] = s[right]
+        s[right]=temp
+        right--
+    }
+
+    return s
 };
