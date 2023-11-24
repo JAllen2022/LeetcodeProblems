30505ce84fbe7acb329c7a0fad0cb2eae8d2fa63
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-11-23 17:35:00 -0800

    Time: 64 ms (57.62%) | Memory: 48.6 MB (38.44%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.js
@@ -6,8 +6,16 @@ var reverseWords = function(s) {
 
     const arr = s.split(" ")
 
+    function reverse(word){
+        let ret=''
+        for(let i= word.length-1; i>=0;i--){
+            ret+=word[i]
+        }
+        return ret
+    }
+
     for(let i=0;i<arr.length;i++) {
-        arr[i] = arr[i].split("").reverse().join("");
+        arr[i] = reverse(arr[i])
     }
     return arr.join(" ")
 };
