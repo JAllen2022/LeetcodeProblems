c15af30e2df74afed0c0aa58c98e384d404bab31
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-11-28 13:32:29 -0800

    Time: 593 ms (5.03%) | Memory: 48.3 MB (5.00%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n, memo={}) {
+    if(n===0) return 0
+    if(n===1) return 1
+    if(memo[n]) return memo[n]
+    memo[n] = fib(n-1) + fib(n-2)
+    return memo[n]
+};
