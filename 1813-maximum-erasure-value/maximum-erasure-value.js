9c959e03ecdb7b74116297fa671e5ed7a75e8792
Jason Allen <109766463+JAllen2022@users.noreply.github.com>
2023-11-29 11:54:08 -0800

    Time: 119 ms (82.46%) | Memory: 56.7 MB (86.84%) - LeetSync

--- 1813-maximum-erasure-value/maximum-erasure-value.js
+++ 1813-maximum-erasure-value/maximum-erasure-value.js
@@ -1,48 +1,26 @@
-// /**
-//  * @param {number[]} nums
-//  * @return {number}
-//  */
-// var maximumUniqueSubarray = function(nums) {
-//    let max = 0;
-//     let runningSum = 0;
-//     let myObj = {};
-//     let left = 0;
-
-//     for (let right = 0; right < nums.length; right++) {
-//         if (myObj[nums[right]] !== undefined && myObj[nums[right]] >= left) {
-//             while (left <= myObj[nums[right]]) {
-//                 runningSum -= nums[left];
-//                 left++;
-//             }
-//         }
-
-//         runningSum += nums[right];
-//         myObj[nums[right]] = right;
-//         max = Math.max(max, runningSum);
-//     }
-
-//     return max;
-    
-// };
-
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var maximumUniqueSubarray = function(nums) {
-    let left = 0
-    let max = 0
-    let sum = 0
-    let set = new Set()
+   let max = 0;
+    let runningSum = 0;
+    let myObj = {};
+    let left = 0;
 
     for (let right = 0; right < nums.length; right++) {
-        while (set.has(nums[right])) {
-            sum -= nums[left]
-            set.delete(nums[left])
-            left++
+        if (myObj[nums[right]] !== undefined && myObj[nums[right]] >= left) {
+            while (left <= myObj[nums[right]]) {
+                runningSum -= nums[left];
+                left++;
+            }
         }
 
-        set.add(nums[right])
-        sum += nums[right]
-
-        max = Math.max(max, sum);
+        runningSum += nums[right];
+        myObj[nums[right]] = right;
+        max = Math.max(max, runningSum);
     }
 
     return max;
+    
 };
